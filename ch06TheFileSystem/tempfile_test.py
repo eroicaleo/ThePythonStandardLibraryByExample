19867fd794cb3ee496cf76d70988ef15b1914bad
Yang Ge <eroicaleo@gmail.com>
2014-07-14 00:39:38 -0700

    Updates on tempfile_test.py

--- ch06TheFileSystem/tempfile_test.py
+++ ch06TheFileSystem/tempfile_test.py
@@ -38,3 +38,33 @@ with tempfile.TemporaryFile(mode='w+t') as temp1:
     temp1.seek(0)
     for line in temp1:
         print(line.rstrip())
+
+with tempfile.NamedTemporaryFile() as temp:
+    print('temp:')
+    print('  ', temp)
+    print('temp.name:')
+    print('  ', temp.name)
+
+print("Exists after close:", os.path.exists(temp.name))
+
+directory_name = tempfile.mkdtemp()
+print(directory_name)
+os.removedirs(directory_name)
+
+with tempfile.NamedTemporaryFile(
+        prefix='prefix_',
+        suffix='_suffix',
+        dir='/tmp') as temp:
+    print('temp:')
+    print('  ', temp)
+    print('temp.name:')
+    print('  ', temp.name)
+
+
+print("tempfile.gettempdir():", tempfile.gettempdir())
+print("tempfile.gettempprefix():", tempfile.gettempprefix())
+
+tempfile.tempdir = '/Ge/Shannon/Melon/Xiangxiang'
+
+print("tempfile.gettempdir():", tempfile.gettempdir())
+print("tempfile.gettempprefix():", tempfile.gettempprefix())
