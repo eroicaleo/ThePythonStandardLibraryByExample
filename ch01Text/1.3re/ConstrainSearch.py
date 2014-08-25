62b38a6868572c47bb40ae82789d235f55b561bc
Yang Ge <eroicaleo@gmail.com>
2014-08-24 23:57:23 -0700

    Updates on ConstrainSearch.py

--- ch01Text/1.3re/ConstrainSearch.py
+++ ch01Text/1.3re/ConstrainSearch.py
@@ -13,4 +13,15 @@ print('Match  :', m)
 s = re.search(pattern, text)
 print('Search :', s)
 
+pattern = re.compile(r'\b\w*is\w*\b')
+print('Text:', text)
 
+pos = 0
+while True:
+    match = pattern.search(text, pos)
+    if not match:
+        break
+    s = match.start()
+    e = match.end()
+    print(' %2d : %2d = "%s"' % (s, e-1, text[s:e]))
+    pos = e
